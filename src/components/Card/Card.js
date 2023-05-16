0923ee17c30e4e06edb4ad3fbddf973248672d5e
Maciej_Zieminski <ziemac1@gmail.com>
2023-05-16 10:22:05 +0200

    14.4

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,37 @@
 import styles from './Card.module.scss';
+import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavourite, removeCard } from '../../redux/store';
+
 
 const Card = props => {
-    return (
-        <li className={styles.card}>{props.title}</li>
-    )
-}
 
-export default Card;
+    const cardId = props.cardId
+    const dispatch = useDispatch();
+
+    const handleFavourite = e => {
+        e.preventDefault();
+        dispatch(toggleCardFavourite(cardId));
+    }
+
+    const handleRemove = e => {
+        e.preventDefault();
+        dispatch(removeCard(cardId));
+    }
+
+    return (
+        <li className={styles.card}>
+            {props.title}
+            <form>
+                <button onClick={handleFavourite}>
+                    <span className={clsx('fa fa-star-o', props.isFavourite && styles.isFavourite)}/>
+                </button> 
+                <button onClick={handleRemove}>
+                    <span className={clsx('fa fa-trash')}/>
+                </button>
+            </form>
+        </li>
+    );
+};
+
+export default Card
